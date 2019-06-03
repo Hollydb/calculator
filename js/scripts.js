1f07f209c01729762afbf98adb200c1575e82388
Holly DeBelser and Shawna Duvall <holly+shawna@>
2019-06-03 11:50:05 -0700

    adds basic math calculation and BMI calculations.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,33 @@
+// Basic Math Calculation
+
 var add = function (num1, num2){
 	return num1 + num2;
 }
+var subtract = function (num1, num2){
+	return num1 - num2;
+}
+var multiply = function (num1, num2){
+	return num1 * num2;
+}
 
+var divide = function (num1, num2){
+	return num1 / num2;
+}
+// Input
 var num1 = parseInt(prompt("Enter a number."));
-var num2 = parseInt(prompt("Enter another numvber."));
+var num2 = parseInt(prompt("Enter another number."));
 
-alert (add(num1, num2));
+var result = divide (num1, num2)
+
+alert (result)
+
+// BMI Calculation
+// var bmi = function (weight, height ) {
+//   return (703 * weight) / (height * height);
+// }
+//
+// var weight = parseFloat(prompt("Enter your weight in pounds:"));
+// var height = parseFloat(prompt ("Enter your height in inches:"));
+//
+// var result = bmi (weight, height).toFixed(1);
+// alert ((result) + " This number actually means NOTHING");
